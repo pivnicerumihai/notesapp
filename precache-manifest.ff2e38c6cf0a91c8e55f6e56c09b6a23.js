752fbbf59a60dbfb1a5a00064b7f1baa9b3324f0
ML Pivniceru <mcpivniceru@gmail.com>
2020-03-01 13:13:36 +0000

    Updates

---
+++ precache-manifest.ff2e38c6cf0a91c8e55f6e56c09b6a23.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1c8cbda40f6459577a19a85bc70a121",
+    "url": "/notesapp/index.html"
+  },
+  {
+    "revision": "cafe6888f960f423d024",
+    "url": "/notesapp/static/js/2.15f3f8a9.chunk.js"
+  },
+  {
+    "revision": "86117cf22341027fcee6",
+    "url": "/notesapp/static/js/main.392a280d.chunk.js"
+  },
+  {
+    "revision": "cef4554ac1798833df7b",
+    "url": "/notesapp/static/js/runtime-main.4c9e26a5.js"
+  }
+]);
